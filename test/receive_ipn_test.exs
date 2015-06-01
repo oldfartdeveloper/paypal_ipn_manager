6e4c37f8b00524607aeb8eeb113d3617dba5d57e
Scott Smith <scottnelsonsmith@gmail.com>
2015-06-01 16:22:28 -0700

    Slowly moving code out of examples into the app's code modules.  Tests are passing

--- test/receive_ipn_test.exs
+++ test/receive_ipn_test.exs
@@ -1,4 +1,4 @@
-defmodule HttpServerTest do
+defmodule ReceiveIpnTest do
   use ExUnit.Case, async: false
 
   setup do
@@ -18,14 +18,8 @@ defmodule HttpServerTest do
   end
 
   test "http server" do
-    assert {:ok, %HTTPoison.Response{body: "", status_code: 200, body: _}} =
-      HTTPoison.get("http://127.0.0.1:5454/entries?list=test&date=20131219")
-
     assert {:ok, %HTTPoison.Response{body: "OK", status_code: 200}} =
-      HTTPoison.post("http://127.0.0.1:5454/add_entry?list=test&date=20131219&title=Dentist", "")
-
-    assert {:ok, %HTTPoison.Response{body: "2013-12-19    Dentist", status_code: 200}} =
-      HTTPoison.get("http://127.0.0.1:5454/entries?list=test&date=20131219")
+      HTTPoison.post("http://127.0.0.1:5454/payments/ipn?list=test&date=20131219&title=Dentist", "")
   end
 
 end
